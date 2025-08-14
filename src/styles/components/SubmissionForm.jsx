d7a6c3d03fb19f54b75aea22269d50eabccf0ae6
Boitumelo <bcrachoshi@gmail.com>
2025-08-15 00:01:12 +0200

    ...

---
+++ src/styles/components/SubmissionForm.jsx
@@ -0,0 +1,10 @@
+// <form> element:
+name="competition-entry"
+method="POST"
+data-netlify="true"
+netlify-honeypot="bot-field"
+
+<input type="hidden" name="form-name" value="competition-entry" />
+<p hidden>
+  <label>Don’t fill this out: <input name="bot-field" /></label>
+</p>
